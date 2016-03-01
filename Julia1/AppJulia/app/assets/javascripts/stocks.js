b8e213d4e9dd48497de5d1b2fc8b2dc8eede8a1b
oliviermenir <oliviermenir@yahoo.com>
2016-03-01 07:58:50 +0100

    Cours 173

--- Julia1/AppJulia/app/assets/javascripts/stocks.js
+++ Julia1/AppJulia/app/assets/javascripts/stocks.js
@@ -15,28 +15,28 @@ init_stock_lookup = function(){
 	hide_spinner();
 	});
 
-$('#stock-lookup-form').on('ajax:success', function(event, data, status){
- 
+	$('#stock-lookup-form').on('ajax:success', function(event, data, status){
+	 
 
-$('#stock-lookup').replaceWith(data);
-init_stock_lookup();
-});
+	$('#stock-lookup').replaceWith(data);
+	init_stock_lookup();
+	});
 
-$('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
+	$('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
 
-hide_spinner();
-$('#stock-lookup-results').replaceWith('');
-$('#stock-lookup-errors').replaceWith('Stock was not found.');
-});
-
-
-}
+	hide_spinner();
+	$('#stock-lookup-results').replaceWith('');
+	$('#stock-lookup-errors').replaceWith('Stock was not found.');
+	});
 
 
+	}
 
-$(document).ready(function() {
-init_stock_lookup();
-})
+
+
+	$(document).ready(function() {
+	init_stock_lookup();
+	})
  
 
 
--- Julia1/AppJulia/config/routes.rb
+++ Julia1/AppJulia/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
+   
   devise_for :users
+
+  resources :user_stocks, except: [:show, :edit, :update]
+
 
   root to: 'pages#home'
 
